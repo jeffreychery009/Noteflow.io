43c1647e73470c48e904e052de260c80b537e591
Jeffrey Chery <jeffchery009@gmail.com>
2024-12-30 02:05:26 -0500

    Developed Auth Form for SignIn and SignUp Pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,13 +9,11 @@ interface AuthLayoutProps {
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ title, children }) => {
   return (
-    <main className="flex justify-center p-6">
+    <main className="flex justify-center">
       <div className="mt-36">
         <h1 className="h2-semibold mb-7 text-center">{title}</h1>
-        <div className="w-[376px] rounded-[20px] border-none bg-light-100 p-4">
-          <div className="mb-24 flex flex-col items-center px-3 py-[52px]">
-            {children}
-          </div>
+        <div className="w-[400px] max-w-lg rounded-[20px] border-none bg-light-100 p-4">
+          <div className="mb-24 flex flex-col px-3 py-[52px]">{children}</div>
           <div>
             <SocialAuthForm />
           </div>
---
+++ components/forms/AuthForm.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+import { z, ZodType } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import ROUTES from "@/constants/routes";
+
+interface AuthFormProps<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  formType: "SIGN_IN" | "SIGN_UP";
+  onSubmit: (data: T) => Promise<{ success: boolean }>;
+}
+
+const AuthForm = <T extends FieldValues>({
+  schema,
+  defaultValues,
+  formType,
+  onSubmit,
+}: AuthFormProps<T>) => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async () => {};
+
+  const buttonText = formType === "SIGN_IN" ? "Sign In" : "Sign Up";
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+        {Object.keys(defaultValues).map((field) => (
+          <FormField
+            key={field}
+            control={form.control}
+            name={field as Path<T>}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  {field.name === "email"
+                    ? "Email Address"
+                    : field.name.charAt(0).toUpperCase() + field.name.slice(1)}
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    required
+                    type={field.name === "password" ? "password" : "text"}
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ))}
+
+        <Button disabled={form.formState.isSubmitting}>
+          {form.formState.isSubmitting
+            ? buttonText === "Sign In"
+              ? "Signing In..."
+              : "Signing Up..."
+            : buttonText}
+        </Button>
+        {formType === "SIGN_IN" ? (
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link href={ROUTES.SIGN_UP}>Sign Up</Link>
+          </p>
+        ) : (
+          <p>
+            Already have an account? <Link href={ROUTES.SIGN_IN}>Sign In</Link>
+          </p>
+        )}
+      </form>
+    </Form>
+  );
+};
+
+export default AuthForm;
