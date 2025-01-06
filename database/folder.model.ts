170666791c5d770e88911e4715a42d0cae07b6cb
Jeffrey Chery <jeffchery009@gmail.com>
2025-01-06 05:51:52 -0500

    Folder Model Created

---
+++ database/folder.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+interface IFolder {
+  ownerId: Schema.Types.ObjectId;
+  name: string;
+  notes: Schema.Types.ObjectId[];
+  sharedWith: Schema.Types.ObjectId[];
+}
+
+const folderSchema = new Schema<IFolder>(
+  {
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    name: { type: String, required: true },
+    notes: [{ type: Schema.Types.ObjectId, ref: "Note", default: [] }],
+    sharedWith: [
+      { type: Schema.Types.ObjectId, ref: "User", index: true, default: [] },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Folder = models?.Folder || model<IFolder>("Folder", folderSchema);
+
+export default Folder;
