4acf0629d2f21c7d0c9cd24b862d53f2e4f394ba
Jeffrey Chery <jeffchery009@gmail.com>
2025-01-06 05:42:09 -0500

    User Model Created

---
+++ database/user.model.ts
@@ -0,0 +1,28 @@
+import { model, models, Schema } from "mongoose";
+
+interface IUser {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+  profileImage: string;
+  createdAt: Date;
+  folders: string[];
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
+    profileImage: { type: String },
+
+    folders: [{ type: Schema.Types.ObjectId, ref: "Folder" }],
+  },
+  { timestamps: true }
+);
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;
