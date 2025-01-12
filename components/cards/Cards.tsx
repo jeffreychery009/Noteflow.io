ca6e1768ce3b0b9865536166ae437e803ac47cc6
Jeffrey Chery <jeffchery009@gmail.com>
2025-01-12 03:21:38 -0500

    Fixed UI for Card Component

--- components/cards/Cards.tsx
+++ components/cards/Cards.tsx
@@ -1,6 +1,6 @@
 const Card = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="border-dark-800 h-[120px] w-full rounded-[12px] border-[0.5px] px-5 py-7 dark:border-dark-700 dark:bg-dark-200 dark:text-white">
+    <div className="min-h-28 w-full rounded-[12px] border-[0.5px] border-dark-800 px-5 py-7 dark:border-dark-700 dark:bg-dark-200 dark:text-white">
       {children}
     </div>
   );
